da3a389f953c29e33ca2e4ae130279225a6dfe71
parthooo <parthoo.rahman@gmail.com>
2023-05-30 20:25:14 +0600

    toggle me nu

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,9 +1,9 @@
 const Link = ({route}) => {
 
     const { name, path} = route;
+    
     return (
-        <li>
-            
+        <li className="mr-12">
             <a href={path}>{name}</a>
         </li>
     );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,38 @@
+import { useState } from 'react';
 import Link from '../Link/Link';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
+
     const routes = [
-        { "id": 1, "name": "home", "path": "/" },
-        { "id": 2, "name": "about", "path": "/about" },
-        { "id": 3, "name": "blog", "path": "/blog" },
-        { "id": 4, "name": "contact", "path": "/contact" },
-        { "id": 5, "name": "products", "path": "/products" }
-    ]
+        { "id": 1, "name": "Home", "path": "/" },
+        { "id": 2, "name": "About", "path": "/about" },
+        { "id": 3, "name": "Blog", "path": "/blog" },
+        { "id": 4, "name": "Contact", "path": "/contact" },
+        { "id": 5, "name": "Products", "path": "/products" }
+    ];
 
     return (
         <nav>
-            <ul className='md:flex'>
-            {
-                routes.map(route => <Link
-                    key={route.id}
-                    route={route}
-                >
-                </Link>)
-            }
+            <div onClick={() => setOpen(!open)} className='md:hidden'>
+                {
+                open === true ? 
+                <XMarkIcon className="h-6 w-6 text-purple-500" /> 
+                : 
+                <Bars3Icon className="h-6 w-6 text-purple-500" />
+                }
+            </div>
+
+            <ul className={`md:flex ${open ? 'top-6' : '-top-96'}`}>
+                {
+                    routes.map(route => <Link
+                        key={route.id}
+                        route={route}
+                    >
+                    </Link>)
+                }
             </ul>
         </nav>
     );
